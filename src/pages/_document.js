18d4a0c48345d5ca3c3ad4b69500f7dfac99225b
chreounsary <chreounsary@gmail.com>
2023-02-08 14:34:35 +0700

    og image

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,18 +1,9 @@
-
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta property="og:locale" content="en_US"></meta>
-        <meta property="og:type" content="article"></meta>
-        <meta name="description" content=""></meta>
-        <meta property="og:title" content=""></meta>
-        <meta property="og:description" content=""></meta>
-        <meta property="og:image" content="https://sciton.info/wp-content/uploads/2023/02/97.jpeg"></meta>
-      </Head>
-
+      <Head />
       <body>
         <Main />
         <NextScript />
