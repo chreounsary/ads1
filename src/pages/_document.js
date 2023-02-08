e7acb437171013e11b9a6b96bd3a1c2700030e6e
chreounsary <chreounsary@gmail.com>
2023-02-08 11:18:16 +0700

    no title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,9 @@ export default function Document() {
     <Html lang="en">
       <meta property="og:locale" content="en_US"></meta>
       <meta property="og:type" content="article"></meta>
-      <meta property="og:site_name" content="scitonpages.vercel.app"></meta>
       <meta name="description" content=""></meta>
       <meta property="og:title" content=""></meta>
       <meta property="og:description" content=""></meta>
-      {/* <meta property="og:url" content="https://scitonpages.vercel.app/detecting-an-underwater-ghost-city-in-the-heart-of-an-84000-year-old-crater-thuhien"></meta> */}
       <meta property="og:image" content="https://sciton.info/wp-content/uploads/2023/02/97.jpeg"></meta>
       <Head />
       <body>
