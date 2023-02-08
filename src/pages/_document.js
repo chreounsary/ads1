f523d956ecfa0a172f0f329e97bbca1ff8cdb47b
chreounsary <chreounsary@gmail.com>
2023-02-08 11:11:07 +0700

    meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <meta property="og:locale" content="en_US"></meta>
+      <meta property="og:type" content="article"></meta>
+      <meta property="og:site_name" content="scitonpages.vercel.app"></meta>
+      <meta name="description" content=""></meta>
+      <meta property="og:title" content="Detecting an underwater ghost city in the heart of an 84,000-year-old crater"></meta>
+      <meta property="og:description" content=""></meta>
+      <meta property="og:url" content="https://scitonpages.vercel.app/detecting-an-underwater-ghost-city-in-the-heart-of-an-84000-year-old-crater-thuhien"></meta>
+      <meta property="og:image" content="https://sciton.info/wp-content/uploads/2023/02/97.jpeg"></meta>
       <Head />
       <body>
         <Main />
