8e5fd9dd56aeb8293d3212a4fd4cd03ab8949dd7
chreounsary <chreounsary@gmail.com>
2023-02-10 16:35:40 +0700

    h

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,34 +8,25 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <Head>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-      <meta charset="utf-8"/>
-      <meta name="viewport" content="width=device-width"/>
-      <link rel="preload" as="image" href="/vercel.svg"/>
-      <link rel="preload" as="image" href="/next.svg"/>
-      <link rel="preload" as="image" href="/thirteen.svg"/>
-      <meta name="next-head-count" content="5"/>
-      <meta property="og:locale" content="en_US"/>
-      <meta property="og:type" content="article"/>
-      <meta name="description" content=""/>
-      <meta property="og:title" content="hello"/>
-      <meta property="og:description" content=""/>
-      <meta property="og:image" content="https://sciton.info/wp-content/uploads/2023/02/97.jpeg"/>
-      <link rel="preload" href="/_next/static/media/2aaf0723e720e8b9.p.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
-      <link rel="preload" href="/_next/static/css/876d048b5dab7c28.css" as="style"/>
-      <link rel="stylesheet" href="/_next/static/css/876d048b5dab7c28.css" data-n-g=""/>
-      <link rel="preload" href="/_next/static/css/77e878d72d98d937.css" as="style"/>
-      <link rel="styles/heet" href="/_next/static/css/77e878d72d98d937.css" data-n-p=""/>
-      <script defer nomodule="" src="/_next/static/chunks/polyfills-c67a75d1b6f99dc8.js">
-      </script><script src="/_next/static/chunks/webpack-8fa1640cc84ba8fe.js" defer></script>
-      <script src="/_next/static/chunks/framework-2c79e2a64abdb08b.js" defer></script>
-      <script src="/_next/static/chunks/main-f11614d8aa7ee555.js" defer></script>
-      <script src="/_next/static/chunks/pages/_app-70d58eb909199b8c.js" defer></script>
-      <script src="/_next/static/chunks/pages/index-7cdff294af1d4061.js" defer></script>
-      <script src="/_next/static/Lfu98fnLrK2cl-sP6LKZs/_buildManifest.js" defer></script>
-      <script src="/_next/static/Lfu98fnLrK2cl-sP6LKZs/_ssgManifest.js" defer></script>
-</Head>
+     <Head>
+        <meta name="description" content="undefined"/>
+        <meta property="og:url" content="https://ads1-eight.vercel.app"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="hello"/>
+        <meta property="og:description" content="undefined"/>
+        <meta property="og:image" content="https://sciton.info/wp-content/uploads/2023/02/97.jpeg"/>
+
+
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="ads1-eight.vercel.app"/>
+        <meta property="twitter:url" content="https://ads1-eight.vercel.app"/>
+        <meta name="twitter:title" content="hello"/>
+        <meta name="twitter:description" content="undefined"/>
+        <meta name="twitter:image" content="https://sciton.info/wp-content/uploads/2023/02/97.jpeg"/>
+        </Head>
+  
+        
+
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
